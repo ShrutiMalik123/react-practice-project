67e314c9f93d0a9edbf6114af73a017f8ac7576a
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-06-25 15:26:16 -0500

    day 1: road to react with ant design

---
+++ road-to-react/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World, my journey with Road to React with Ant Design</h1>
+    </div>
+  );
+}
+
+export default App;
