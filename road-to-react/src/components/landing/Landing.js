e9326fcffba07b925e993cc8b42a6b368b8c6f20
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-06-25 16:55:43 -0500

    added component landing page/router not working

---
+++ road-to-react/src/components/landing/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Hello World, my journey with Road to React with Ant Design</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
