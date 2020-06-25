217fb43a897c2898f037a6642d1986e3652e62aa
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2020-06-25 17:02:38 -0500

    landing page working but router not working

--- road-to-react/src/App.js
+++ road-to-react/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import LandingPage from "./components/landing/Landing";
 function App() {
   return (
     <div>
       <h1>Hello World, my journey with Road to React with Ant Design</h1>
+      <LandingPage />
     </div>
   );
 }
